5483fb3a2c0b40607c53f11d68184dcaa6bf27a9
ILS-DMinh <117742444+ILS-DMinh@users.noreply.github.com>
2025-10-01 22:51:23 +0700

    HW1

---
+++ HW1/Introduction/src/main/java/org/example/Introduction.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Le Duy Minh" + '\t' + "24020231" + '\t' + "IT6" + '\t' + "Minhleduy" + '\t' + "minhle040526@gmail.com");
+        for(int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
